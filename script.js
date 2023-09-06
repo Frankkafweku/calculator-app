b92d99aca8484c5f77bbb6321fdb6dee3b86d178
Frank Kafweku <frankkafweku84@gmail.com>
2023-09-06 19:41:16 +0200

    Fully functional basic calculator

--- script.js
+++ script.js
@@ -44,8 +44,24 @@ keys.addEventListener('click', e => {
 
         if (action === "calculate"){
             const firstValue = calculator.dataset.firstValue
-            const operatorc = calculator.dataset.operator
+            const operator = calculator.dataset.operator
             const secondValue = displayedNum
+
+            const calculate = (n1, operator, n2) => {
+                let result = ''
+                
+                if (operator === 'add') {
+                  result = parseFloat(n1) + parseFloat(n2)
+                } else if (operator === 'subtract') {
+                  result = parseFloat(n1) - parseFloat(n2)
+                } else if (operator === 'multiply') {
+                  result = parseFloat(n1) * parseFloat(n2)
+                } else if (operator === 'divide') {
+                  result = parseFloat(n1) / parseFloat(n2)
+                }
+                
+                return result
+              }
             results.textContent = calculate(firstValue, operator, secondValue)
         }
 
